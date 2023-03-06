0f5d14c781676967d428c38565e9b19a75be5e31
Abdülsamet YILMAZ <mr.asmtylmz@gmail.com>
2023-03-06 19:54:58 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Dynamic;
+using System.Transactions;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************************************");
+            Console.WriteLine("       Teklik çiftlik sorugulama");
+            Console.WriteLine("*************************************");
+            int sayi =Convert.ToInt32(Console.ReadLine());
+
+            if ((sayi % 2) == 0)
+            {
+                Console.WriteLine("Girilen değer çift sayıdır \a");
+            }
+
+            else
+            {
+                Console.WriteLine("Girilen değer tek sayıdır \a" );
+            }
+
+        }
+    }
+
+} 
